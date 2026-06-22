5a0cc43228bc91cd2f1887e65cb75d14c1883991
rbogamerr <120429144+rbogamerr@users.noreply.github.com>
2026-06-23 01:00:09 +0200

    Delete src directory

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://vrkkidechcegqiklfzpv.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZya2tpZGVjaGNlZ3Fpa2xmenB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDkwNDYsImV4cCI6MjA5NzcyNTA0Nn0.cfqh9L2wSadFcqzWnopmUizLq4aIlG34QfrvJEOWqZ8";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
